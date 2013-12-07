9392ce8cfa731dba41d4b4a88a52dcb748ca47f8
unknown <antoine.drabble@gmail.com>
2013-12-07 03:03:51 +0100

    Add login and search buttons

--- app/cache/dev/twig/34/fc/01f957a4c89c66c722987869ae67897d66fc433ef8a6de2ca0518bda1282.php
+++ app/cache/dev/twig/34/fc/01f957a4c89c66c722987869ae67897d66fc433ef8a6de2ca0518bda1282.php
@@ -62,11 +62,22 @@ class __TwigTemplate_34fc01f957a4c89c66c722987869ae67897d66fc433ef8a6de2ca0518bd
 \t<body>
 \t\t<div class=\"container\">
 \t\t\t<div class=\"row\">
-\t\t\t  <div class=\"col-md-3\">
+\t\t\t  <div class=\"col-md-7\">
 \t\t\t\t<h1><a href=\"/\">Portfolio</a></h1>
 \t\t\t  </div>
-\t\t\t  <div class=\"col-md-9\">
-\t\t\t\t<br />
+\t\t\t  <div class=\"col-md-3\">
+\t\t\t\t\t<br/>
+\t\t\t\t\t<form class=\"navbar-form\" role=\"search\">
+\t\t\t\t\t  <div class=\"input-group\">
+\t\t\t\t\t\t<input type=\"text\" class=\"form-control\" placeholder=\"Search\" name=\"srch-term\" id=\"srch-term\">
+\t\t\t\t\t\t<div class=\"input-group-btn\">
+\t\t\t\t\t\t  <button class=\"btn btn-default\" type=\"submit\"><span class=\"glyphicon icon-search\"></span></button>
+\t\t\t\t\t\t</div>
+\t\t\t\t\t  </div>
+\t\t\t\t\t</form>
+\t\t\t\t</div>
+\t\t\t  <div class=\"col-md-2\">
+\t\t\t\t<br /><br/>
 \t\t\t\t<p class=\"pull-right\">
 \t\t\t\t\t<a href=\"#\"><i rel=\"tooltip\" data-original-title=\"Twitter\" class=\"icon-twitter\"></i></a>
 \t\t\t\t\t<a href=\"#\"><i rel=\"tooltip\" data-original-title=\"Facebook\" class=\"icon-facebook\"></i></a>
@@ -81,43 +92,71 @@ class __TwigTemplate_34fc01f957a4c89c66c722987869ae67897d66fc433ef8a6de2ca0518bd
 \t\t\t  <div class=\"col-md-6\">
 \t\t\t\t<ul class=\"nav nav-tabs\">
 \t\t\t\t\t<li id=\"menu\" ";
-        // line 40
+        // line 51
         if (((isset($context["active"]) ? $context["active"] : $this->getContext($context, "active")) == "index")) {
             echo "class=\"active_page\"";
         }
         echo "><a href=\"/Symfony/web/app_dev.php/\">Home</a></li>
 \t\t\t\t\t<li id=\"menu\" ";
-        // line 41
+        // line 52
         if (((isset($context["active"]) ? $context["active"] : $this->getContext($context, "active")) == "studies")) {
             echo " class=\"active_page\"";
         }
         echo "><a href=\"/Symfony/web/app_dev.php/studies\">Studies</a></li>
 \t\t\t\t\t<li id=\"menu\" ";
-        // line 42
+        // line 53
         if (((isset($context["active"]) ? $context["active"] : $this->getContext($context, "active")) == "articles")) {
             echo " class=\"active_page\"";
         }
         echo "><a href=\"/Symfony/web/app_dev.php/articles\">Articles</a></li>
 \t\t\t\t\t<li id=\"menu\" ";
-        // line 43
+        // line 54
         if (((isset($context["active"]) ? $context["active"] : $this->getContext($context, "active")) == "contact")) {
             echo " class=\"active_page\"";
         }
         echo "><a href=\"/Symfony/web/app_dev.php/contact\">Contact</a></li>
 \t\t\t\t\t<li id=\"menu\" ";
-        // line 44
+        // line 55
         if (((isset($context["active"]) ? $context["active"] : $this->getContext($context, "active")) == "admin")) {
             echo " class=\"active_page\"";
         }
         echo "><a href=\"/Symfony/web/app_dev.php/admin\">Admin</a></li>
+\t\t\t\t\t<li class=\"dropdown\">
+\t\t\t\t\t\t<a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">Login<b class=\"caret\"></b></a>
+\t\t\t\t\t\t<ul class=\"dropdown-menu unstyled pull-left dropdown-features\">
+\t\t\t\t\t\t\t<div class=\"panel panel-default\">
+\t\t\t\t\t\t\t\t<div class=\"panel-heading\">
+\t\t\t\t\t\t\t\t\t<h3 class=\"panel-title\">Please sign in</h3>
+\t\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t\t<div class=\"panel-body\">
+\t\t\t\t\t\t\t\t\t<form accept-charset=\"UTF-8\" role=\"form\">
+\t\t\t\t\t\t\t\t\t<fieldset>
+\t\t\t\t\t\t\t\t\t\t<div class=\"form-group\">
+\t\t\t\t\t\t\t\t\t\t\t<input class=\"form-control\" placeholder=\"E-mail\" name=\"email\" type=\"text\">
+\t\t\t\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t\t\t\t<div class=\"form-group\">
+\t\t\t\t\t\t\t\t\t\t\t<input class=\"form-control\" placeholder=\"Password\" name=\"password\" type=\"password\" value=\"\">
+\t\t\t\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t\t\t\t<div class=\"checkbox\">
+\t\t\t\t\t\t\t\t\t\t\t<label>
+\t\t\t\t\t\t\t\t\t\t\t\t<input name=\"remember\" type=\"checkbox\" value=\"Remember Me\"> Remember Me
+\t\t\t\t\t\t\t\t\t\t\t</label>
+\t\t\t\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t\t\t\t<input class=\"btn btn-lg btn-primary btn-block\" type=\"submit\" value=\"Login\">
+\t\t\t\t\t\t\t\t\t</fieldset>
+\t\t\t\t\t\t\t\t\t</form>
+\t\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t</ul>
+\t\t\t\t\t</li>
 \t\t\t\t</ul>
 \t\t\t  </div>
 \t\t\t</div><br/>
  
     ";
-        // line 49
+        // line 88
         $this->displayBlock('body', $context, $blocks);
-        // line 51
+        // line 90
         echo "\t\t\t<br />
 \t\t\t<br />
 \t\t\t<hr />
@@ -131,10 +170,10 @@ class __TwigTemplate_34fc01f957a4c89c66c722987869ae67897d66fc433ef8a6de2ca0518bd
 </html>";
     }
 
-    // line 49
+    // line 88
     public function block_body($context, array $blocks = array())
     {
-        // line 50
+        // line 89
         echo "    ";
     }
 
@@ -150,6 +189,6 @@ class __TwigTemplate_34fc01f957a4c89c66c722987869ae67897d66fc433ef8a6de2ca0518bd
 
     public function getDebugInfo()
     {
-        return array (  138 => 50,  135 => 49,  121 => 51,  119 => 49,  109 => 44,  103 => 43,  97 => 42,  91 => 41,  85 => 40,  59 => 17,  55 => 16,  51 => 15,  47 => 14,  41 => 11,  37 => 10,  33 => 9,  20 => 1,  63 => 18,  58 => 16,  53 => 14,  48 => 12,  42 => 8,  39 => 7,  32 => 4,  29 => 8,);
+        return array (  177 => 89,  174 => 88,  160 => 90,  158 => 88,  120 => 55,  114 => 54,  108 => 53,  102 => 52,  96 => 51,  59 => 17,  55 => 16,  51 => 15,  47 => 14,  41 => 11,  37 => 10,  33 => 9,  29 => 8,  20 => 1,);
     }
 }
